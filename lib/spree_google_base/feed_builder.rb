c2360acaba2a99ef6281b391b023b3809a937ae7
Sherry Son <son.sherrysoomin92@gmail.com>
2016-05-25 15:37:14 -0400

    the feed builder has been configured to render the correct information for glossier

--- lib/spree_google_base/feed_builder.rb
+++ lib/spree_google_base/feed_builder.rb
@@ -3,116 +3,123 @@ require 'net/ftp'
 module SpreeGoogleBase
   class FeedBuilder
     include Spree::Core::Engine.routes.url_helpers
-    
+
     attr_reader :store, :domain, :title
-    
+
     def self.generate_and_transfer
       self.builders.each do |builder|
         builder.generate_and_transfer_store
       end
     end
 
-    def self.generate_test_file(file_path)
+    def self.generate_test_file(filename)
       exporter = new
-
-      File.open(file_path, "w") do |file|
+      exporter.instance_variable_set("@filename", filename)
+      File.open(exporter.path, "w") do |file|
         exporter.generate_xml file
       end
+      exporter.path
     end
 
     def self.builders
-      if defined?(Spree::Store)
-        Spree::Store.all.map{ |store| self.new(:store => store) }
-      else
-        [self.new]
-      end
+      [self.new]
     end
 
     def initialize(opts = {})
-      raise "Please pass a public address as the second argument, or configure :public_path in Spree::GoogleBase::Config" unless
+      raise "Please pass a public address as the second argument, or configure :public_domain in Spree::GoogleBase::Config" unless
         opts[:store].present? or (opts[:path].present? or Spree::GoogleBase::Config[:public_domain])
 
-      @store = opts[:store] if opts[:store].present?
-      @title = @store ? @store.name : Spree::GoogleBase::Config[:store_name]
-      
-      @domain = @store ? @store.domains.match(/[\w\.]+/).to_s : opts[:path]
-      @domain ||= Spree::GoogleBase::Config[:public_domain]
+      #@title = Spree::GoogleBase::Config[:store_name]
+      @title = "Glossier Product Feed"
+
+      #@domain = Spree::Config[:site_url]
+      @domain = "https://www.glossier.com/#!"
     end
-    
+
     def ar_scope
-      if @store
-        Spree::Product.by_store(@store).google_base_scope
-      else
-        Spree::Product.google_base_scope
-      end
+      Spree::Product.all
     end
 
     def generate_and_transfer_store
       delete_xml_if_exists
 
-      File.open(path, 'w') do |file| 
+      File.open(path, 'w') do |file|
         generate_xml file
       end
 
       transfer_xml
       cleanup_xml
     end
-    
+
     def path
-      "#{::Rails.root}/tmp/#{filename}"
+      file_path = Rails.root.join('/tmp')
+      if defined?(Apartment)
+        file_path = file_path.join(Apartment::Tenant.current_tenant)
+        FileUtils.mkdir_p(file_path)
+      end
+      file_path.join(filename)
     end
-    
+
     def filename
-      "google_base_v#{@store.try(:code)}.xml"
+      @filename ||= "google_base.xml"
     end
 
     def delete_xml_if_exists
       File.delete(path) if File.exists?(path)
     end
 
-    def generate_xml output
-      xml = Builder::XmlMarkup.new(:target => output)
+    def generate_xml(output)
+      xml = Builder::XmlMarkup.new(target: output, indent: 2)
       xml.instruct!
 
       xml.rss(:version => '2.0', :"xmlns:g" => "http://base.google.com/ns/1.0") do
         xml.channel do
           build_meta(xml)
-          
-          ar_scope.find_each(:batch_size => 300) do |product|
-            build_product(xml, product)
+
+          # NOTE: product.hide_from_emails != true (to include nil or false)
+          # was added to prevent future changes to the database to result in
+          # the creation of an incorrect feed
+
+          ar_scope.find_each() do |product|
+            variants = Spree::Variant.find_each()
+            variants.each do |item|
+              if (item.product_id == product.id && !item.gtin.nil?) && (product.hide_from_emails != true)
+                build_product(xml, product, item)
+              end
+            end
           end
         end
       end
     end
-    
+
     def transfer_xml
       raise "Please configure your Google Base :ftp_username and :ftp_password by configuring Spree::GoogleBase::Config" unless
         Spree::GoogleBase::Config[:ftp_username] and Spree::GoogleBase::Config[:ftp_password]
-      
+
       ftp = Net::FTP.new('uploads.google.com')
       ftp.passive = true
       ftp.login(Spree::GoogleBase::Config[:ftp_username], Spree::GoogleBase::Config[:ftp_password])
       ftp.put(path, filename)
       ftp.quit
     end
-    
+
     def cleanup_xml
       File.delete(path)
     end
-    
-    def build_product(xml, product)
+
+    def build_product(xml, product, variant)
       xml.item do
-        xml.tag!('link', product_url(product.permalink, :host => domain))
-        build_images(xml, product)
-        
+        xml.tag!('link', product_url(product.slug, host: domain).gsub("spree/", ""))
+        build_images(xml, product, variant)
+
         GOOGLE_BASE_ATTR_MAP.each do |k, v|
-          value = product.send(v)
-          xml.tag!(k, value.to_s) if value.present?
+          value = variant.send(v)
+          xml.tag!(k, value.to_s)
         end
       end
     end
-    
-    def build_images(xml, product)
+
+    def build_images(xml, product, variant)
       if Spree::GoogleBase::Config[:enable_additional_images]
         main_image, *more_images = product.master.images
       else
@@ -120,18 +127,20 @@ module SpreeGoogleBase
       end
 
       return unless main_image
-      xml.tag!('g:image_link', image_url(product, main_image))
+      xml.tag!('g:image_link', main_image.attachment.url)
 
       if Spree::GoogleBase::Config[:enable_additional_images]
         more_images.each do |image|
-          xml.tag!('g:additional_image_link', image_url(product, image))
+          xml.tag!('g:additional_image_link', image_url(variant, image))
         end
       end
     end
 
-    def image_url product, image
-      base_url = image.attachment.url(product.google_base_image_size)
-      base_url = "#{domain}/#{base_url}" unless Spree::Config[:use_s3]
+    def image_url(variant, image)
+      base_url = image.attachment.url(variant.google_base_image_size)
+      if Spree::Image.attachment_definitions[:attachment][:storage] != :s3
+        base_url = "#{domain}#{base_url}"
+      end
 
       base_url
     end
@@ -140,6 +149,6 @@ module SpreeGoogleBase
       xml.title @title
       xml.link @domain
     end
-    
+
   end
 end
